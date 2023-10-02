51e6035d2ed52b84daf7aca150f436db903282ca
Mehmet <mehmetsonmez216@gmail.com>
2023-10-02 04:00:07 +0300

    finished

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Karakter from "./components/Karakter";
+import Names from "./components/Names";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [characters, setCharacters] = useState([]);
+
+  // Make a request for a user with a given ID
+  useEffect(() => {
+    axios
+      .get("https://swapi.dev/api/people/")
+      .then(function (response) {
+        // handle success
+        console.log(response);
+        setCharacters(response.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+  }, []);
 
   return (
     <div className="App">
       <h1 className="Header">Karakterler</h1>
+      {characters.map((karakter, i) => (
+        <Names key={i} title={karakter.name}>
+          <Karakter karakter={karakter} />
+        </Names>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Karakter.js
+++ src/components/Karakter.js
@@ -1,1 +1,17 @@
-// Karakter bileşeniniz buraya gelecek
+import React from "react";
+const Karakter = ({ karakter }) => {
+  return (
+    <div className="karakter">
+      <p>gender: {karakter.gender}</p>
+      <p>Height: {karakter.height}</p>
+      <p>Mass: {karakter.mass}</p>
+      <p>BirtYear: {karakter.birth_year}</p>
+      <p>Eye Color: {karakter.eye_color}</p>
+      <p>Hair Color: {karakter.hair_color}</p>
+      <p>Skin Color: {karakter.skin_color}</p>
+      <p>Films: {karakter.films}</p>
+    </div>
+  );
+};
+
+export default Karakter;
